3badc7d6cb38f06fbf5d1c380bb224aaa0797b36
luzar <luzar@LAPTOP-RISQ0JN3>
2025-01-08 15:55:12 -0200

    tentativa number 3

--- Projeto_Java_2/src/PackageNumber2/ClassJava2.java
+++ Projeto_Java_2/src/PackageNumber2/ClassJava2.java
@@ -6,6 +6,15 @@ public class ClassJava2 {
 	 
 	 private String  sobrenome ;
 
+	 private int idade ;
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
 	public String getNome() {
 		return nome;
 	}
